30efd55d66d9fc3bf21b0737e0f562c9537427af
ndagirelillian <ndagirelillian15@gmail.com>
2025-06-09 16:11:21 +0300

    static

--- orchard/settings.py
+++ orchard/settings.py
@@ -160,12 +160,16 @@ STATIC_URL = 'static/'
 # STATICFILES_DIRS = [
 #      os.path.join(BASE_DIR, 'static'),
 #  ]
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = '/var/www/orchard/static'
 
 
-MEDIA_URL = 'media/'
+# Media files (User uploads, etc.)
+MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = '/var/www/orchard/media'
+
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
