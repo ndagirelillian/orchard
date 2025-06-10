6dd87bfaaf561d0f65801a48f7f6e2f4b9e30a8f
ndagirelillian <ndagirelillian15@gmail.com>
2025-06-10 12:50:21 +0300

    static

--- orchard/settings.py
+++ orchard/settings.py
@@ -156,13 +156,13 @@ LOGOUT_REDIRECT_URL = '/user/login/'
 STATIC_URL = 'static/'
 # STATICFILES_DIRS = [
 #      os.path.join(BASE_DIR, 'static'),
-#  ]
-STATIC_ROOT = BASE_DIR/'staticfiles'
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = 'media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
