7fef7cee14674b51012c447850970d45d348fba6
ndagirelillian <ndagirelillian15@gmail.com>
2025-06-10 13:12:50 +0300

    static2

--- orchard/settings.py
+++ orchard/settings.py
@@ -154,15 +154,12 @@ LOGOUT_REDIRECT_URL = '/user/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#      os.path.join(BASE_DIR, 'static'),
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+# STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 MEDIA_URL = 'media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR/'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
